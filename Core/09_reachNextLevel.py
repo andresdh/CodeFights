37009d4d8fdc25184947d48f5ded4a4774f09439
Andres del Hoyo <andres.delhoyo@gmail.com>
2018-08-29 11:38:18 -0300

    Core/Cursos

---
+++ Core/09_reachNextLevel.py
@@ -0,0 +1,27 @@
+# You are playing an RPG game. Currently your experience points (XP) total is
+# equal to experience. To reach the next level your XP should be at least at
+#  threshold. If you kill the monster in front of you, you will gain more
+#   experience points in the amount of the reward.
+#
+# Given values experience, threshold and reward, check if you reach the next
+#  level after killing the monster.
+#
+# Example
+#
+# For experience = 10, threshold = 15, and reward = 5, the output should be
+# reachNextLevel(experience, threshold, reward) = true;
+# For experience = 10, threshold = 15, and reward = 4, the output should be
+# reachNextLevel(experience, threshold, reward) = false.
+
+experience = 97
+threshold = 57
+reward = 7
+
+def reachNextLevel(experience, threshold, reward):
+    flg = False
+    if (reward + experience) >= threshold:
+        flg = True
+
+    return flg
+
+print(reachNextLevel(experience, threshold, reward))
